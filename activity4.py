a5ca1232b33fbd8f452d12800c40eba2171acedd
ryasun85 <59563060+ryasun85@users.noreply.github.com>
2020-01-21 14:36:38 +0000

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,15 @@
+space1 = "x"
+space2 = "0"
+
+print(("\033[1;32;40m   |   |"))
+print(" {} | {} |".format(space1,space2) )
+print(("   |   |"))
+print(("-------------"))
+print(("   |   |"))
+print(" {} | {} |".format(space1, space1) )
+print(("   |   |"))
+print(("-------------"))
+print(("   |   |"))
+print(" {} |   |".format(space2))
+print(("   |   |"))
+
